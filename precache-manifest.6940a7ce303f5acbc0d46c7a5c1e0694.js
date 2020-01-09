988180b72a2052c6bdc9df132b86f006235feec0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-09 11:52:41 +0000

    Deploy neopepes.com to github.com/safudex/neopepes.git:gh-pages

---
+++ precache-manifest.6940a7ce303f5acbc0d46c7a5c1e0694.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020b2f0ab815bb61e47fd18df0b687ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b56cf9a4058df2cdd921",
+    "url": "/static/css/2.b8f3649f.chunk.css"
+  },
+  {
+    "revision": "4dca76f02dfe05be378c",
+    "url": "/static/css/3.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "e2d3ffe6dc3e2cbde3f0",
+    "url": "/static/css/4.45c2a69a.chunk.css"
+  },
+  {
+    "revision": "44e0fc33e6732c9f92dd",
+    "url": "/static/css/5.70025abf.chunk.css"
+  },
+  {
+    "revision": "e7d9cf4a3d892796144d",
+    "url": "/static/css/main.9d11692a.chunk.css"
+  },
+  {
+    "revision": "b56cf9a4058df2cdd921",
+    "url": "/static/js/2.15fa3f50.chunk.js"
+  },
+  {
+    "revision": "828c60abc8dffff8d12bf4f1c238136a",
+    "url": "/static/js/2.15fa3f50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dca76f02dfe05be378c",
+    "url": "/static/js/3.aca7b8a1.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/3.aca7b8a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2d3ffe6dc3e2cbde3f0",
+    "url": "/static/js/4.a78d2da2.chunk.js"
+  },
+  {
+    "revision": "44e0fc33e6732c9f92dd",
+    "url": "/static/js/5.e6e9fcac.chunk.js"
+  },
+  {
+    "revision": "e7d9cf4a3d892796144d",
+    "url": "/static/js/main.4c6cd375.chunk.js"
+  },
+  {
+    "revision": "216b4135965637894205",
+    "url": "/static/js/runtime-main.cccbb6c7.js"
+  },
+  {
+    "revision": "21f6235482ecdf67217dfa18483f7716",
+    "url": "/static/media/NotoSans-Regular.21f62354.ttf"
+  },
+  {
+    "revision": "6876078902e180c2045aa6c44c9d3ea7",
+    "url": "/static/media/pepe.68760789.png"
+  }
+]);

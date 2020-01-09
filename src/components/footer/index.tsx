fb05b37212b17bc7f73b2a74ae418f2da6c83100
Albert <github@albert.sh>
2020-01-09 18:05:50 +0700

    Remove footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,58 +4,6 @@ import style from './style.module.css';
 import zilliqaImg from '../../assets/images/zilliqa.png';
 const copyright: string = 'Copyright © Zilliqa 2019';
 
-const Footer: React.SFC = () => (
-  <footer className={style.footer}>
-    <div className="text-center">
-      <span className={style.copyright}>{copyright}</span>
-      <ul className="nav justify-content-center">
-        <li className="nav-item">
-          <a
-            className="text-secondary nav-link"
-            href="https://zilliqa.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label={'Zilliqa Homepage'}
-          >
-            <img className={style.zilliqa} src={zilliqaImg} alt="Zilliqa Logo" />
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="text-secondary nav-link"
-            href="https://github.com/Zilliqa"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label={'Zilliqa GitHub'}
-          >
-            <FaGithub />
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="text-secondary nav-link"
-            href="https://gitter.im/Zilliqa"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label={'Zilliqa Gitter'}
-          >
-            <FaGitter />
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            className="text-secondary nav-link"
-            href="https://twitter.com/zilliqa"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label={'Zilliqa Twitter'}
-          >
-            <FaTwitter />
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
-);
+const Footer: React.SFC = () => null;
 
 export default Footer;
